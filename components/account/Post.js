7e929fbd21f697494ba688b4b0ef150f52e020b9
chicken2609@hotmail.com <lesbianhaircut@outlook.com>
2023-03-19 00:23:02 -0400

    c

--- components/account/Post.js
+++ components/account/Post.js
@@ -1,11 +1,10 @@
 import { useState } from "react";
 import styled from "styled-components";
 import COLORS from "../../data/colors";
-import Post from "./Post";
 const Cont = styled.div``;
 
-const RecentPosts = ({ posts }) => {
+const Post = ({ posts }) => {
   return <Cont colors={COLORS}></Cont>;
 };
 
-export default RecentPosts;
+export default Post;
