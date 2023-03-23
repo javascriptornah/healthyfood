e0f8cdf0af1679af9b7992e2da689b4dc37a984c
Matthew Pierce <84364656+miscany@users.noreply.github.com>
2023-03-23 17:39:21 +0000

    c

--- components/forum/Header.js
+++ components/forum/Header.js
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styled from "styled-components";
 import COLORS from "../../data/colors";
 import Signup from "./Signup";
 import SignupPopup from "../popups/SignupPopup";
+import supabase from "../../utils/supabaseClient";
 
 const Cont = styled.div``;
 const Header = () => {
@@ -14,14 +15,32 @@ const Header = () => {
   const hideLogin = () => {
     setShowLogin(false);
   };
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    const fetchUser = async () => {
+      const { data: session } = await supabase.auth.getSession();
+      if (session.session != null) {
+        setUser(session.session.user);
+      }
+    };
+    fetchUser();
+  }, []);
+  console.log(user);
   return (
-    <Cont colors={COLORS} className="mar-bottom-32">
+    <Cont colors={COLORS} className="mar-bottom-48">
       {showLogin && <SignupPopup hideFunction={hideLogin} />}
 
       <h3 className="underline text-shadow-red mar-bottom-16">
         FOOD SOURCING FORUM
       </h3>
-      <Signup showFunction={displayLogin} />
+      {user !== null ? (
+        <p>
+          You are currently signed in as{" "}
+          <span className="light-blue-2">{user.username} </span>
+        </p>
+      ) : (
+        <Signup showFunction={displayLogin} />
+      )}
     </Cont>
   );
 };
