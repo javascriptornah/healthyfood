a8d7a3b3751aaaca069d20099d8b6cd48ace329e
chicken2609@hotmail.com <lesbianhaircut@outlook.com>
2023-05-03 21:03:03 -0400

    c

--- components/feed/Location.js
+++ components/feed/Location.js
@@ -1,9 +1,17 @@
+import Image from "next/image";
 import { useState } from "react";
 import styled from "styled-components";
 import COLORS from "../../data/colors";
 
 const Cont = styled.div``;
-const Location = () => {
+const Location = ({
+  name,
+  country,
+  state,
+  created_at,
+  description,
+  images,
+}) => {
   return <Cont colors={COLORS}></Cont>;
 };
 
